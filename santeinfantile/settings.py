f7940303ceb0ab629e3166fe19a09a55964b0826
SOSSA GNINAZÉ MINGNISSÊ DARIUS <sante.infantile.benin@gmail.com>
2026-04-26 15:33:38 +0100

    Fix CSRF Railway

--- santeinfantile/settings.py
+++ santeinfantile/settings.py
@@ -80,3 +80,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
+CSRF_TRUSTED_ORIGINS = [
+    'https://sante-infantile-benin-production.up.railway.app'
+]
