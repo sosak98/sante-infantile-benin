48d21850d484438e0febe2c35c9da44ebdc8fb74
SOSSA GNINAZÉ MINGNISSÊ DARIUS <sante.infantile.benin@gmail.com>
2026-05-02 00:39:44 +0100

    Fix carte affichage

--- sante/views.py
+++ sante/views.py
@@ -1,6 +1,11 @@
+import json
 from django.shortcuts import render
 from .models import Etablissement
 
 def carte(request):
-    etablissements = list(Etablissement.objects.values())  # conversion en liste de dictionnaires
-    return render(request, 'sante/carte.html', {'etablissements': etablissements})
+    etablissements = list(Etablissement.objects.values(
+        'nom', 'adresse', 'telephone', 'latitude', 'longitude', 'type_etab'
+    ))
+    return render(request, 'sante/carte.html', {
+        'etablissements_json': json.dumps(etablissements)
+    })
