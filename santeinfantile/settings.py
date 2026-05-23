77ebc6b55d5e527e9f1fb417697971328533d1f1
SOSSA GNINAZÉ MINGNISSÊ DARIUS <sante.infantile.benin@gmail.com>
2026-05-23 14:26:19 +0100

    Nouveau design magnifique

--- santeinfantile/settings.py
+++ santeinfantile/settings.py
@@ -74,6 +74,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'sante' / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
